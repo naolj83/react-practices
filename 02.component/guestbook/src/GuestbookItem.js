9cdef4a6ec9a54de96a6c59d5bf69f1fb63f2193
naolj83 <naolj83@gmail.com>
2021-07-29 09:34:03 +0900

    guestbook 수정

--- 02.component/guestbook/src/GuestbookItem.js
+++ 02.component/guestbook/src/GuestbookItem.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 export default function GuestbookList({name, message, regDate}) {
         return (
                 <li className='Guestbook__List__Item'>
                     <strong>{name}</strong>
-                    <p>{message}</p>
+                    <p>
+                { message && message.split('\n').map((line, index) => index > 0 ?
+                    <Fragment>
+                        <br/>
+                        { line }
+                    </Fragment> : line) }
+            </p>
                     <strong>{regDate}</strong>
                     <a href=''>삭제</a>
                 </li>
