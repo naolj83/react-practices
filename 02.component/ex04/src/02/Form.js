1290e3433b27eb4343b3fa9c744ba3dc421f4fb3
naolj83 <naolj83@gmail.com>
2021-08-02 11:53:19 +0900

    제어 컴포넌트 #3

--- 02.component/ex04/src/02/Form.js
+++ 02.component/ex04/src/02/Form.js
@@ -1,14 +1,35 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import './assets/Form.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+
+
 
 export default function Form() {
     const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [validEmail, setValidEmail] = useState(false);
+    const [gender, setGender] = useState('female');
+    const [birthYear, setBirthYear] = useState(1984);
+    const [selfDescription, setSelfDescription] = useState('');
 
     const onChangeInputName = function(e) {
         // setName(e.target.value);
         // 10자 제한(validation)
         setName(e.target.value.substr(0, 10));
-    }
+    };
+
+    const onChangeInputEmail = function(e) {
+        setEmail(e.target.value);
+
+        const re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+        setValidEmail(re.test(e.target.value));
+
+    };
+
+    const onChangeInputGender = function(e) {
+        setGender(e.target.value);
+    };
 
     return (
         <form id="joinForm" name="joinForm" method="post" action="/do/not/post">
@@ -21,30 +42,41 @@ export default function Form() {
                 onChange={ onChangeInputName } />
 
             <label htmlFor="email">이메일</label>
-            <input id="email" name="email" type="text" value={ "" }/>
-
+            <input 
+                id="email" 
+                name="email" 
+                type="text" 
+                value={ email } 
+                onChange={ onChangeInputEmail } />
+                {
+                    email === '' ?
+                        null :
+                        validEmail ?
+                            <FontAwesomeIcon icon={ faCheckCircle } style={ {marginLeft: 5, fontSize: 16, color: 'green'} } /> :
+                            <FontAwesomeIcon icon={ faTimesCircle } style={ {marginLeft: 5, fontSize: 16, color: 'red'} } />
+                }
             <label htmlFor="password">패스워드</label>
             <input id="password" name="password" type="password" value={ "" } />
 
             <fieldset>
                 <legend>성별</legend>
-                <label>여</label> <input type="radio" name="gender" value={ "female" } defaultChecked={ true } />
-                <label>남</label> <input type="radio" name="gender" value={ "male" } defaultChecked={ false } />
+                <label>여</label> <input type="radio" name="gender" value={ "female" } checked={ gender === 'female' } onChange={ onChangeInputGender } />
+                <label>남</label> <input type="radio" name="gender" value={ "male" } checked={ gender === 'male' } onChange={ onChangeInputGender } />
             </fieldset>
 
             <label htmlFor="birthYear">생년</label>
-            <select id="birthYear">
-                <option value='1984'>1984년</option>
-                <option value='1985'>1985년</option>
-                <option value='1986'>1986년</option>
-                <option value='1987'>1987년</option>
-                <option value='1988'>1988년</option>
-                <option value='1989'>1989년</option>
-                <option value='1990'>1990년</option>
+            <select id="birthYear" value={ birthYear } onChange={ e => setBirthYear(e.target.value)}>
+                <option value={ 1984 }>1984년</option>
+                <option value={ 1985 }>1985년</option>
+                <option value={ 1986 }>1986년</option>
+                <option value={ 1987 }>1987년</option>
+                <option value={ 1988 }>1988년</option>
+                <option value={ 1989 }>1989년</option>
+                <option value={ 1990 }>1990년</option>
             </select>
 
             <label htmlFor="birthYear">자기소개</label>
-            <textarea value={""} />
+            <textarea id="selfDescription" name="selfDescription" value={ selfDescription } onChange={ e => setSelfDescription(e.target.value)} />
 
             <fieldset>
                 <legend>약관동의</legend>
