eaf3c5ad042e8ade4b14e4d4c6df1f84e6c6dcf3
naolj83 <naolj83@gmail.com>
2021-08-05 11:01:03 +0900

    stateful component & callback pattern #2

--- 02.component/ex04/src/04/Emaillist.js
+++ 02.component/ex04/src/04/Emaillist.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import Email from './Email';
+import PropTypes from 'prop-types';
 
-export default function Emaillist({ emails }) {
+export default function Emaillist({ keyword, emails }) {
+    console.log('rendering.... Emaillist');
     return (
         <ul className={ 'Emaillist' }>
             { 
-                emails.map(item => <Email 
+                emails
+                    .filter(item => item.firstName.indexOf(keyword) != -1 || item.lastName.indexOf(keyword) != -1 || item.email.indexOf(keyword) != -1)
+                    .map(item => <Email
                                         key={ item.no }
-                                        firstName={ item.firstName }
-                                        lastName={ item.lastName }
-                                        email={ item.email }     />)
+                                        firstName={item.firstName}
+                                        lastName={item.lastName}
+                                        email={item.email} />)
             }
-        </ul>
-);
+        </ul>        
+    );    
+}
+
+Emaillist.propTypes = {
+    emails: PropTypes.arrayOf(PropTypes.shape(Email.propTypes)) 
 }
